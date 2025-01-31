4ff790a93bd9dfe1b002965865e7e707695001b7
Watta2xTops <geRivera@mcm.edu.ph>
2025-01-31 19:16:37 +0800

    Added more files and rename main

---
+++ script.js
@@ -0,0 +1,10 @@
+const navigateButton1 = document.getElementById('PHtravel');
+const navigateButton2 = document.getElementById('course');
+
+navigateButton1.addEventListener('click', function () {
+    window.location.href = 'https://github.com/Watta2xTops/Watta2xTops.github.io/RiveraExplorePHTours.html'; 
+});
+
+navigateButton2.addEventListener('click', function () {
+    window.location.href = 'https://github.com/Watta2xTops/Watta2xTops.github.io/RiveraCourseSchedule.html'; 
+});
