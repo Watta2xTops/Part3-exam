0a15ec9e38ab2ff2add44ade4d98a109b5b86250
Watta2xTops <geRivera@mcm.edu.ph>
2025-01-31 21:22:47 +0800

    new link method

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const navigateButton1 = document.getElementById('PHtravel');
+const navigateButton1 = document.getElementById('PHtravels');
 const navigateButton2 = document.getElementById('course');
 
 navigateButton1.addEventListener('click', function () {
-    window.location.href = 'https://watta2xtops.github.io/Part3-exam/RiveraExplorePHTours.html'; 
+    window.open('https://watta2xtops.github.io/Part3-exam/RiveraExplorePHTours.html', '_blank');
 });
 
 navigateButton2.addEventListener('click', function () {
-    window.location.href = 'https://watta2xtops.github.io/Part3-exam/RiveraCourseSchedule.html'; 
+    window.open('https://watta2xtops.github.io/Part3-exam/RiveraCourseSchedule.html', '_blank');
 });
+
