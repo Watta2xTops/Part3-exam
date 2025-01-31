a90af1841b2e7cce6e734939384ea01a35774aa2
Watta2xTops <geRivera@mcm.edu.ph>
2025-01-31 20:31:25 +0800

    Try linking

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const navigateButton1 = document.getElementById('PHtravel');
 const navigateButton2 = document.getElementById('course');
 
 navigateButton1.addEventListener('click', function () {
-    window.location.href = 'RiveraExplorePHTours.html'; 
+    window.location.href = 'https://watta2xtops.github.io/Part3-exam/RiveraExplorePHTours.html'; 
 });
 
 navigateButton2.addEventListener('click', function () {
-    window.location.href = 'RiveraCourseSchedule.html'; 
+    window.location.href = 'https://watta2xtops.github.io/Part3-exam/RiveraCourseSchedule.html'; 
 });
