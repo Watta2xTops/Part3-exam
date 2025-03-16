ea184c737d6d6643faf57f347254e6e527981110
Watta2xTops <geRivera@mcm.edu.ph>
2025-03-16 21:01:05 +0800

    Added Content

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const navigateButton1 = document.getElementById('PHtravels');
 const navigateButton2 = document.getElementById('course');
+const navigateButton3 = document.getElementById('gitRepos');
 
 navigateButton1.addEventListener('click', function () {
     window.open('https://watta2xtops.github.io/Part3-exam/RiveraExplorePHTours.html', '_blank');
@@ -7,5 +8,8 @@ navigateButton1.addEventListener('click', function () {
 
 navigateButton2.addEventListener('click', function () {
     window.open('https://watta2xtops.github.io/Part3-exam/RiveraCourseSchedule.html', '_blank');
+});
+navigateButton3.addEventListener('click', function() {
+    window.open('https://github.com/vennDiagramm/Monterde_Apartments_Dormitory-Manager', '_blank');
 });
 
