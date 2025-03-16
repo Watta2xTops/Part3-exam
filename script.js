1898c3c55318e2264c5d525f2bb5e978e9364661
Watta2xTops <geRivera@mcm.edu.ph>
2025-03-16 21:13:09 +0800

    Fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navigateButton1 = document.getElementById('PHtravels');
 const navigateButton2 = document.getElementById('course');
-const navigateButton3 = document.getElementById('gitRepos');
+const navigateButton3 = document.getElementById('gitRepo');
 
 navigateButton1.addEventListener('click', function () {
     window.open('https://watta2xtops.github.io/Part3-exam/RiveraExplorePHTours.html', '_blank');
